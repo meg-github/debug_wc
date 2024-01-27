26691fcf9dcf6ac08a9d9c412475e2087402a534
Megumi Yasuo <meg-github@users.noreply.github.com>
2024-01-27 16:23:42 +0900

    initial commit
    initial commit

---
+++ debug.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from janome.tokenizer import Tokenizer
+import collections
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud
+
+text = "これはテスト用のテキストです．この解析結果がワードクラウドによって表示されるはずです"
+
+tk = Tokenizer(wakati=True)
+tokens = tk.tokenize(text)
+words = " ".join(list(tokens))
+
+wc = WordCloud(font_path = "SourceHanSerifK-Light.otf").generate(words)
+
+
+
+fig = plt.figure(figsize=(12,9))
+ax = plt.axes()
+st.set_option('deprecation.showPyplotGlobalUse', True)
+plt.tight_layout()
+plt.imshow(wc,interpolation="bilinear")
+plt.axis("off")
+st.pyplot(fig)
