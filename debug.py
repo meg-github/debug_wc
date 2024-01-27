65f9e47f61512895dfd572d8ee359c2b9eac858a
Megumi Yasuo <meg-github@users.noreply.github.com>
2024-01-27 17:47:23 +0900

    debug
    debug

--- debug.py
+++ debug.py
@@ -22,3 +22,20 @@ plt.tight_layout()
 plt.imshow(wc,interpolation="bilinear")
 plt.axis("off")
 st.pyplot(fig)
+
+
+import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Create some sample text
+text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
+
+# Create and generate a word cloud image:
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+st.pyplot()
